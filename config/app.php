0f9a130265f0101f0b2d39fea23cd5c15542980b
Minescorpio <jonathan14morice@gmail.com>
2025-01-08 10:02:15 +0100

    #28 - Added currency configuration and helpers 💶

--- config/app.php
+++ config/app.php
@@ -78,11 +78,15 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => 'fr',
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => 'en',
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => 'fr_FR',
+
+    'currency' => 'EUR',
+
+    'currency_symbol' => '€',
 
     /*
     |--------------------------------------------------------------------------
